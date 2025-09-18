c4c288e2062fd68977b676c12e5bf5fcbc81ee68
Gesiel <gesiel@gkstech.co>
2025-09-18 20:36:43 -0300

    Add tidewave

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule FinaGrana.MixProject do
       {:ecto_sql, "~> 3.13"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_reload, "~> 1.6.1", only: :dev},
       {:phoenix_live_view, "~> 1.1.3"},
       {:lazy_html, ">= 0.1.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
@@ -63,7 +63,8 @@ defmodule FinaGrana.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:tidewave, "~> 0.5", only: :dev}
     ]
   end
 
